065c798818a627e8c2805d3b59aea231b19c8d4b
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-11-18 10:11:18 -0800

    setup: remove random index.js files

--- packages/two/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var greetings = require("one");
-var printGreetings = function () {
-    console.log("Greetings from everywhere! ");
-    console.log(greetings.heyFunction());
-};
