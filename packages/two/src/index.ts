a9c6982165500f89edafe0a15433ea379fa83294
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-11-18 10:13:40 -0800

    setup: update index.ts

--- packages/two/src/index.ts
+++ packages/two/src/index.ts
@@ -2,7 +2,7 @@ import * as greetings from "one";
 
 const printGreetings = () => {
   console.log("Greetings from everywhere! ");
-  console.log(greetings.heyFunction);
+  console.log(greetings.heyFunction());
 };
 
 console.log(printGreetings());
